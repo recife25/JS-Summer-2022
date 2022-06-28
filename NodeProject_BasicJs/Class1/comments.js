717e0ab5751cd56b21204cfae4309fa899f4861e
recife25 <felipe.j.ruiz64@gmail.com>
2022-06-28 19:48:58 -0400

    added comments js file

--- NodeProject_BasicJs/Class1/comments.js
+++ NodeProject_BasicJs/Class1/comments.js
@@ -8,6 +8,40 @@ console.log('Im hacking with visual studio code editor')
 
 
 
+// anything after double-slash will be considered as comments (not code) on this line
+console.log('Hello World!')     // console.log statement helps us to print into in the terminal
+console.log('ABCD     .....      EFGH')
+
+/*
+    This is
+
+    a way
+
+
+    to add
+
+    multi line
+
+
+    comments
+*/
+
+/**
+ * this is
+ * 
+ * another way
+ * 
+ * to write
+ * 
+ * multi line
+ * 
+ * 
+ * comments
+ * 
+ */
+
+
+
 
 
 
