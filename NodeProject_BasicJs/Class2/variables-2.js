a7cba11b3ac05a67bc1b6d0a025878e8cca843ae
recife25 <felipe.j.ruiz64@gmail.com>
2022-06-28 22:20:52 -0400

    class2 files

---
+++ NodeProject_BasicJs/Class2/variables-2.js
@@ -0,0 +1,76 @@
+let myName = null; // create a variable (myName) and stored value (null);
+console.log(myName);
+/**
+ * 
+ * undefined - which is undefined
+ * 
+ * null - empty or nothing
+ * 
+ */
+
+//to store 'happy' in the myName variable 
+myName = 'Happy';
+console.log(myName);
+myName = 'laugh';
+console.log(myName);
+/**
+ * Datatypes in Javascript:
+ * 
+ * number -> all digits are consider as numbers (with/without decimal point)
+ * 
+ * string -> any value enclosed in single or double quotes 
+ * eg: 
+ *  'hello'
+ *  "hello"
+ *  "HELLO WORLD"
+ *  'Hello World'
+ * 
+ * use of single quotes is common practice in js-development
+ * 
+ * boolean -> true or false 
+ * 
+ * Array -> can store one or more values at a time 
+ * let planet1 = 'Earth';
+ * let planet2 = 'Jupiter';
+ * let planet3 = 'Saturn';
+ * let planet4 = 'Venus';
+ * let planet5 = 'Mercury';
+ * let planet6 = 'Mars';
+ * let planet7 = 'Uranus';
+ * let planet8 = 'Neptune';
+ * 
+ * let planets = ['Earth', 'Jupiter', 'Saturn, 'Venus', 'Mercury', 'Mars', 'Uranus', 'Neptune'];
+ * 
+ * object -> holds key-value pair 
+ * eg:
+ * var student1 = {
+ *      name: 'Felipe'
+ *      location: 'New York'
+ *      gender: 'Male'
+*       courses: 'QA'
+        paymentType: 'paid full'
+ * }
+ * 
+ * 
+ * var student2 = {
+ *      name: 'Liz'
+ *      location: 'NJ'
+ *      gender: 'Female'
+ *      courses: 'life coach'
+ *      paymentType: 'payment plan'
+ * 
+ * }
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
+
+let planets = ['Earth', 'Jupiter', 'Saturn', 'Venus', 'Mercury', 'Mars', 'Uranus', 'Neptune'];
+
+console.log(planets);
