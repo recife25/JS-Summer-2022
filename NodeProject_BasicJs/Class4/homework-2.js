747a522a495d38e07e006094254fa93b9c10bf12
recife25 <felipe.j.ruiz64@gmail.com>
2022-07-07 20:54:49 -0400

    added new analysis and solutions to code

--- NodeProject_BasicJs/Class4/homework-2.js
+++ NodeProject_BasicJs/Class4/homework-2.js
@@ -24,14 +24,9 @@ const sentence1 = 'Hello dear, how are you doing?';
 //const sentence1 = 'Felipe J. Ruiz (Phil)'
 let result1 = 0;
 let sentence2 = sentence1.length;
-console.log(sentence2);
-// if (sentence2 >= 10)
-//     result1 = 15;
-// else
-//     result1 = 25;
-//iteration for loop
+console.log(`\nSentence1 : The lenght of sentence-1 equals :${sentence2}`);
 sentence2 = sentence2 >= 10 ? result1 = 15 : result1 = 25; 
-console.log(`result1  ${result1}`);
+console.log(`result1 ${result1}`);
 
 //const sentence2 = "HeAlTh wAs EArlIer said To Be the AbILitY of the bOdY funcTiOnInG WElL.";
 /**
@@ -40,7 +35,7 @@ console.log(`result1  ${result1}`);
 
 const sentence3 = "heAlth wAs eArlier said to be the AbIlitY of the bOdY funcTiOnInG WElL.";
 let sentence_Replace_All_a_Z = sentence3.replace(/A/g, 'Alpha');
-console.log(sentence_Replace_All_a_Z);
+console.log(`\nSentence-2 : a/A replace with Alpha : ${sentence_Replace_All_a_Z}`);
 
 
 
@@ -54,19 +49,58 @@ console.log(sentence_Replace_All_a_Z);
  * 5. the last-character in sentence-3
  * 6. word 'Body' is present only once. (true or false)
  */
+//3.1 - The length of sentence-3
 const sentence4 = "HeAlTh wAs EArlIer said To Be the AbILitY of the bOdY funcTiOnInG WElL.";
 let sentence5 = sentence4.length;
-console.log(sentence5);
-let healthMsgs = 'HeAlth wAs EArlIer said To Be the AbILitY of the bOdY funcTiOnInG WElL.';
-let indexof_h = healthMsgs.indexOf('h');
-console.log(`\nindexof_H ${indexof_h}`);
-let indexof_B = healthMsgs.indexOf('b');
-console.log(`\nindexof_Body ${indexof_B}`);
-let lastIndexof_Body = healthMsgs.lastIndexOf('body');
-console.log(`\nlastIndesxof_Body ${lastIndexof_Body}`);
-let bodyName = 'bOdY'
-let bodyNameIncludes_bO = bodyName.includes('bOdY');
-console.log(`\nbodyNameIncludes_bO ${bodyNameIncludes_bO}`);
+console.log(`\nSentence-3.1 : The length of sentence-3 equals :${sentence5}`);
+//3.2 - Does sentence-3 starts with 'health' (ignore cases)
+let checkStarts = 'health';
+let healthlowerCase = checkStarts.toLowerCase();
+let sentence3_Lowercase = sentence3.toLowerCase();
+let includeresult = sentence3_Lowercase.startsWith(healthlowerCase);
+console.log(`Sentence-3.2 : startsWith "${checkStarts}" : ${includeresult}`)
+
+//3.3 - Does sentence-3 contains with 'Body" (ignore cases)
+checkContains = 'Body';
+let bodyupperCase = checkContains.toUpperCase();
+let sentence3_upperCase = sentence3.toUpperCase();
+let includeResult = sentence3_upperCase.includes(sentence3_upperCase);
+console.log(`Sentence-3.3 : contains "${checkContains}" : ${includeResult}`);
+
+//3.4 Index of 'Body' in sentence-3
+let indexBody = 'Body';
+let bodyLowerCase = indexBody.toLowerCase();
+let indexof_Body = bodyLowerCase.indexOf('Body');
+let indexof_Body1 = bodyLowerCase.indexOf('y');
+console.log(`Sentence-3.4 : indexof 'Body' "${indexof_Body}" indexof 'y' "${indexof_Body1}"`);
+
+//3.5 last-character in sentence-3
+const sentence6 = "HeAlTh wAs EArlIer said To Be the AbILitY of the bOdY funcTiOnInG WElL.";
+let lastChar = sentence6.charAt(-1);
+console.log(`Sentence-3.5 : lastChar ${lastChar}`);
+
+//3.6 word 'Body' is present only once T/F 
+let s3LowerCase = sentence3.toLowerCase();
+let word_Body = 'Body';
+let word_Body_LC = word_Body.toLowerCase();
+let indexOfBody = s3LowerCase.indexOf();
+let lastindexofBody = s3LowerCase.lastIndexOf();
+indexOfBody === lastindexofBody && indexBody >= 0;
+console.log(`${lastindexofBody} and ${indexBody}`);
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
 
 
 
