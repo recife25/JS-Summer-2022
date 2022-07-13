3815bb4498dd6a18ff01833de084628158581b4a
recife25 <felipe.j.ruiz64@gmail.com>
2022-07-13 07:44:37 -0400

    added hw3 q3 q4 1/2 answers im rusing here

--- NodeProject_BasicJs/Class5/homework-3.js
+++ NodeProject_BasicJs/Class5/homework-3.js
@@ -35,33 +35,27 @@ console.log(`Q2 - Count the number of words > ${countWords}`);
   * 
   */
 
-const word = ('have');
-let split_word0 = (word.split(' '));
-let first_letterUC = (word.substring(0, 1).toUpperCase());
-let rest_letterLC = (word.substring(1).toLowerCase());
-let concat = (first_letterUC.concat(rest_letterLC));
+let statement3 = 'have a great day'
+let split_word0 = (statement3.split(' '));
+let word0 = split_word0[0].substring(0, 1).toUpperCase();
+let word_0 = split_word0[0].substring(1).toLowerCase();
+let concat_words0 = word0.concat(word_0);
 
-const word1 = ('a');
-let split_word1 = (word1.split(' '));
-let single_letterUC1 = (word1.substring(0, 1).toUpperCase());
-
-const word2 = ('great');
-let split_word2 = (word2.split(' '));
-let single_letterUC2 = (word2.substring(0, 1).toUpperCase());
-let rest_letterLC1 = (word2.substring(1).toLowerCase());
-let concat1 = (single_letterUC2.concat(rest_letterLC1));
-
-const word3 = ('day');
-let split_word3 = (word3.split(' '));
-let single_letterUC3 = (word3.substring(0, 1).toUpperCase());
-let rest_letterLC3 = (word3.substring(1).toLowerCase());
-let concat2 = (single_letterUC3.concat(rest_letterLC3));
-console.log(`${concat} ${single_letterUC1} ${concat1} ${concat2}`);
-            
-            
-
-
-
+let split_word1 = (statement3.split(' '));
+let word1 = split_word0[1].substring(0, 1).toUpperCase();
+let word_1 = split_word1[1].substring(1).toLowerCase();
+let concat_words1 = word1.concat(word_1);
+ 
+let split_word2 = (statement3.split(' '));
+let word2 = split_word0[2].substring(0, 1).toUpperCase();
+let word_2 = split_word0[2].substring(1).toLowerCase();
+let concat_words2 = word2.concat(word_2);
+ 
+let split_word3 = (statement3.split(' '));
+let word3 = split_word0[3].substring(0, 1).toUpperCase();
+let word_3 = split_word0[3].substring(1).toLowerCase();
+let concat_words3 = word3.concat(word_3);
+console.log(`Q3 - uppercase first letters : ${concat_words0}  ${concat_words1}  ${concat_words2}  ${concat_words3}`);
  
  /**
   * Q4:
@@ -72,8 +66,22 @@ console.log(`${concat} ${single_letterUC1} ${concat1} ${concat2}`);
   * 'yOu neVER WaLK alOne'   ->  'YNWA'
   * 
   */
- 
- 
- 
- 
- 
+let statement = 'have a great day';
+let split_result = statement.split(' ');
+console.log(split_result);
+let words0 = split_result[0].substring(0, 1).toUpperCase();
+let words1 = split_result[1].substring(0, 1).toUpperCase();
+let words2 = split_result[2].substring(0, 1).toUpperCase();
+let words3 = split_result[3].substring(0, 1).toUpperCase();
+let concat_abbrev = words0.concat(words1, words2, words3);
+console.log(`Q4 - abbreviation : ${concat_abbrev}`);
+
+
+  
+
+
+  
+
+
+
+
